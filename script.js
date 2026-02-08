0276d97e0beecd9c8f767d91de1dc1328c9cbc9c
matheswaran <matheswaranr07@gmail.com>
2026-02-08 22:26:29 +0530

    fix: update worker URL to ageotherplanet.ageotherplanet.workers.dev

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-fetch("https://ageotherplanet.rvw.workers.dev")
+fetch("https://ageotherplanet.ageotherplanet.workers.dev")
     .then(res => res.json())
     .then(data => {
         document.getElementById("output").innerText = data.message;
     })
     .catch(err => {
         console.error(err);
-        document.getElementById("output").innerText = "Error loading data";
+        document.getElementById("output").innerText = "Error loading data (Backend might be waking up...)";
     });
